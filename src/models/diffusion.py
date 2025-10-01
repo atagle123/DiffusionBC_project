a7ea2c8539644cadbc81072d3257ef96e6b77f17
augusto <augusto.tagle@uc.cl>
2025-09-30 22:30:41 -0300

    added  repaint diffusion base code

--- src/models/diffusion.py
+++ src/models/diffusion.py
@@ -417,3 +417,203 @@ class FiLMGaussianDiffusion(GaussianDiffusion):
         losses = self.p_losses(x, condition, t)
         self.model.clear_conditioning()
         return losses
+
+
+class GaussianDiffusionRePaint(GaussianDiffusion):
+    """
+    TODO: add repaint sampling
+    """
+
+    def __init__(
+        self,
+        model: nn.Module,
+        data_dim: int,
+        schedule: str = "cosine",
+        n_timesteps: int = 15,
+        compile: bool = False,
+    ):
+        self.model = model
+        if compile:
+            self.model.compile()
+        self.data_dim = data_dim
+
+        schedulers = {
+            "cosine": cosine_beta_schedule,
+        }
+
+        if schedule in schedulers:
+            betas = schedulers[schedule](n_timesteps)
+        else:
+            raise ValueError(
+                f"Unknown schedule: {schedule}. Available options are 'vp', 'cosine', or 'linear'."
+            )
+
+        alphas = 1.0 - betas
+        alphas_cumprod = torch.cumprod(alphas, axis=0)
+        alphas_cumprod_prev = torch.cat([torch.ones(1), alphas_cumprod[:-1]])
+
+        sqrt_alphas = torch.sqrt(alphas)
+
+        self.n_timesteps = int(n_timesteps)
+
+        register_buffer = lambda name, val: self.register_buffer(
+            name, val.to(torch.float32)
+        )  # helper function to register buffer from float64 to float32
+
+        register_buffer("sqrt_alphas", sqrt_alphas)
+
+        register_buffer("betas", betas)
+        register_buffer("alphas_cumprod", alphas_cumprod)
+        register_buffer("alphas_cumprod_prev", alphas_cumprod_prev)
+
+        # calculations for diffusion q(x_t | x_{t-1}) and others
+        register_buffer("sqrt_alphas_cumprod", torch.sqrt(alphas_cumprod))
+        register_buffer(
+            "sqrt_one_minus_alphas_cumprod", torch.sqrt(1.0 - alphas_cumprod)
+        )
+        register_buffer("log_one_minus_alphas_cumprod", torch.log(1.0 - alphas_cumprod))
+        register_buffer("sqrt_recip_alphas_cumprod", torch.sqrt(1.0 / alphas_cumprod))
+        register_buffer(
+            "sqrt_recipm1_alphas_cumprod", torch.sqrt(1.0 / alphas_cumprod - 1)
+        )
+
+        # calculations for posterior q(x_{t-1} | x_t, x_0)
+        posterior_variance = (
+            betas * (1.0 - alphas_cumprod_prev) / (1.0 - alphas_cumprod)
+        )
+        register_buffer("posterior_variance", posterior_variance)
+
+        ## log calculation clipped because the posterior variance
+        ## is 0 at the beginning of the diffusion chain
+        register_buffer(
+            "posterior_log_variance_clipped",
+            torch.log(torch.clamp(posterior_variance, min=1e-20)),
+        )
+        register_buffer(
+            "posterior_mean_coef1",
+            betas * np.sqrt(alphas_cumprod_prev) / (1.0 - alphas_cumprod),
+        )
+        register_buffer(
+            "posterior_mean_coef2",
+            (1.0 - alphas_cumprod_prev) * np.sqrt(alphas) / (1.0 - alphas_cumprod),
+        )
+
+    @torch.inference_mode()
+    def p_mean_variance(self, x, t):
+        epsilon = self.model(x=x, time=t, training=False)
+        if self.repaint_sampling:
+            # Repaint like sampling
+
+        x_recon = self.predict_start_from_noise(x, t=t, noise=epsilon)
+
+        if self.clip_denoised:
+            x_recon.clamp_(-1.0, 1.0)
+
+        model_mean, posterior_variance, posterior_log_variance = self.q_posterior(
+            x_start=x_recon, x_t=x, t=t
+        )
+        return model_mean, posterior_variance, posterior_log_variance
+
+    def p_sample(self, x, t):
+        b, *_, device = *x.shape, x.device
+
+        batched_time = torch.full((b,), t, device=device, dtype=torch.long)
+
+        model_mean, _, model_log_variance = self.p_mean_variance(x=x, t=batched_time)
+
+        if t > 0:
+            noise = torch.randn_like(x) * self.temperature
+        else:
+            noise = 0
+
+        x_pred = model_mean + (0.5 * model_log_variance).exp() * noise
+
+        return x_pred
+
+    def p_sample_loop(self, shape: tuple[int]) -> Sample:
+        """
+        Classical DDPM sampling algorithm
+
+            Parameters:
+                condition
+                shape,
+
+            Returns:
+            sample
+
+        """
+
+        device = self.betas.device
+
+        x = torch.randn(shape, device=device) * self.temperature
+
+        chain = [x] if self.return_chain else None
+
+        for t in tqdm(
+            reversed(range(0, self.n_timesteps)),
+            desc="sampling loop time step",
+            total=self.n_timesteps,
+            disable=self.disable_progess_bar,
+        ):
+            x = self.p_sample(x=x, t=t)
+            if self.return_chain:
+                chain.append(x)
+
+        if self.return_chain:
+            chain = torch.stack(chain, dim=1)
+
+        if self.clip_denoised:
+            x.clamp_(-1.0, 1.0)
+
+        return Sample(x, chain)
+
+    @torch.no_grad()
+    def forward(self, batch_size: int, mask, known_data) -> Sample:
+        """ """
+
+        return self.p_sample_loop(shape=(batch_size, self.data_dim))
+
+    def setup_sampling(
+        self,
+        clip_denoised=True,
+        temperature=1,
+        disable_progess_bar=True,
+        return_chain=False,
+        guidance_scale=0,  # if 0 no class free guidance...
+        **kwargs,
+    ):
+        self.temperature = temperature
+        self.clip_denoised = clip_denoised
+        self.disable_progess_bar = disable_progess_bar
+        self.return_chain = return_chain
+        self.guidance_scale = guidance_scale
+
+    # ------------------------------------------ training ------------------------------------------#
+
+    def q_sample(self, x_start, t, noise):
+        sample = (
+            extract(self.sqrt_alphas_cumprod, t, x_start.shape) * x_start
+            + extract(self.sqrt_one_minus_alphas_cumprod, t, x_start.shape) * noise
+        )
+
+        return sample
+
+    def p_losses(self, x_start, t):
+        noise = torch.randn_like(x_start)
+
+        x_noisy = self.q_sample(x_start=x_start, t=t, noise=noise)
+
+        pred_epsilon = self.model(x_noisy, t)
+
+        assert noise.shape == pred_epsilon.shape
+
+        loss = F.mse_loss(pred_epsilon, noise, reduction="none").mean()
+
+        return loss, {"loss": loss}
+
+    def loss(self, x):
+        batch_size = len(x)
+
+        t = torch.randint(0, self.n_timesteps, (batch_size,), device=x.device).long()
+
+        return self.p_losses(x, t)
